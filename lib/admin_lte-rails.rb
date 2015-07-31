050eb153d3700426ef2f79b0f3ca446942622489
Sameer Rahmani <lxsameer@gnu.org>
2015-07-31 16:13:12 +0430

    minor bug fixed

--- lib/admin_lte-rails.rb
+++ lib/admin_lte-rails.rb
@@ -1,18 +1,18 @@
 require 'admin_lte-rails/version'
 require 'bootstrap-sass'
-require 'rails-assets-admin-lte'
 require 'jquery-rails'
 require 'font-awesome-rails'
 require 'jquery-ui-rails'
 require 'select2-rails'
 require 'momentjs-rails'
+require 'chart-js-rails'
+require 'ionicons-rails'
 require 'bootstrap3-datetimepicker-rails'
 require 'rails-assets-jquery-knob'
 require 'rails-assets-bootstrap-daterangepicker'
 require 'rails-assets-jquery-sparkline'
 require 'rails-assets-jquery-icheck'
-require 'chart-js-rails'
-require 'ionicons-rails'
+require 'rails-assets-admin-lte'
 
 module AdminLte
 
--- lib/admin_lte-rails/version.rb
+++ lib/admin_lte-rails/version.rb
@@ -1,3 +1,3 @@
 module AdminLte
-  VERSION = '2.2.0.3'
+  VERSION = '2.2.0.4'
 end
